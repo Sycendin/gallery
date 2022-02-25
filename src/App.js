6b27643016f8ef6be23a2456df0d84b6cb59ac08
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-02-24 23:56:48 -0500

    when clicking image, modal pops up with bigger image and some text

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import "./App.css";
 import React, { useState } from "react";
 import Modal from "./components/Modal";
 function App() {
+  function changeInfo(info) {
+    modalInfo = info;
+  }
   const [isOpen, setIsOpen] = useState(false);
+  let modalInfo = "";
   return (
     <div className="App">
       <div className="Button-wrapper">
         <button onClick={() => setIsOpen(true)}>Open Modal</button>
-        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+        <img
+          className="gallery-image"
+          onMouseOver={changeInfo(
+            "https://cdn.discordapp.com/attachments/788247984517283880/933260764351963206/MikeTroutFace.png"
+          )}
+          onClick={() => setIsOpen(true)}
+          style={{ height: 300, width: 200 }}
+          src="https://cdn.discordapp.com/attachments/788247984517283880/933260764351963206/MikeTroutFace.png"
+          alt="mike"
+        ></img>
+        <Modal open={isOpen} info={modalInfo} onClose={() => setIsOpen(false)}>
           Modal
         </Modal>
       </div>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,14 +2,24 @@ import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import "./Modal.css";
 
-export default function Modal({ open, children, onClose }) {
+export default function Modal({ open, onClose, info }) {
   if (!open) return null;
   return ReactDOM.createPortal(
     <Fragment>
       <div className="Overlay-styles"></div>
+
       <div className="Modal-style">
-        <button onClick={onClose}>Close Modal</button>
-        {children}
+        <div className="Modal-close"></div>
+        <img
+          className="image"
+          style={{ height: 500, width: 300 }}
+          src={info}
+          alt="face"
+        ></img>
+        <p>This is a drawn image of Mike Trout's face</p>
+        <button className="Modal-button" onClick={onClose}>
+          Close Modal
+        </button>
       </div>
     </Fragment>,
     document.getElementById("portal")
