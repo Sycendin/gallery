411318517faf6d935ebdcd41f03643d446dc2d79
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-03-01 00:51:10 -0500

    added about page and routes to go to it and home page

--- src/App.js
+++ src/App.js
@@ -4,32 +4,41 @@ import Modal from "./components/Modal";
 import Images from "./Images";
 import CardList from "./components/Cardlist";
 import Nav from "./components/Nav";
+import About from "./components/About";
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [changedInfo, setInfo] = useState([]);
+  const [route, setRoute] = useState("home");
 
   const infoChange = (event) => {
     setInfo(event);
   };
-
+  const routeChange = (path) => {
+    setRoute(path);
+  };
   return (
     <Fragment>
-      <Nav></Nav>
-      <h1 className="flex justify-center bg-light-red">
-        Click on an image for more info
-      </h1>
-      <div className="App">
-        <div className="Button-wrapper">
-          <Modal
-            open={isOpen}
-            info={changedInfo}
-            onClose={() => setIsOpen(false)}
-          >
-            Modal
-          </Modal>
+      <Nav routePass={routeChange}></Nav>
+
+      {route === "home" ? (
+        <div className="App">
+          <h1 className="flex justify-center bg-light-red">
+            Click on an image for more info
+          </h1>
+          <div className="Button-wrapper">
+            <Modal
+              open={isOpen}
+              info={changedInfo}
+              onClose={() => setIsOpen(false)}
+            >
+              Modal
+            </Modal>
+          </div>
+          <CardList open={setIsOpen} changeinfo={infoChange} images={Images} />
         </div>
-        <CardList open={setIsOpen} changeinfo={infoChange} images={Images} />
-      </div>
+      ) : route === "about" ? (
+        <About></About>
+      ) : null}
     </Fragment>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,7 @@
 import React, { Fragment } from "react";
-// enter properties
-// destructure from props in the parameters
+
 const Card = ({ day, text, link, directLink, change, open }) => {
-  // destructure from props
-  // const {name, email, id} = props;
-  // change("something");
   return (
-    // use Fragment so we don't need to wrap everything in <div>
     <Fragment>
       <div className="bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5">
         <img
