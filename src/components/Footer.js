b27001ebb172d1ad01bf90e3c0e83b75390b753e
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-03-05 00:33:13 -0500

    added images to public folder and compressed them and reduced resolution for faster load rather than using links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 const Footer = () => {
   return (
     <Fragment>
-      <div className="absolute w-100 bottom-0 bg-light-red white-80 pv1 pv1-l ph1">
+      <div className="fixed w-100 bottom-0 bg-light-red white-80 pv1 pv1-l ph1">
         <p className="f6">
           <span className="dib mr4 mr5-ns">©2048 Your Company LLC, Inc.</span>
           <a className="link white-80 hover-light-purple" href="/terms">
