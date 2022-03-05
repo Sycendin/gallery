adcd867eda87d36fc14c69a5d98c677210a91a4e
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-03-05 02:11:15 -0500

    improved progressive app checks by changing maximum-scale and changing css

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,15 @@ import React, { Fragment } from "react";
 const Card = ({ day, text, link, directLink, change, open }) => {
   return (
     <Fragment>
-      <div className="bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5 w-40 h-40">
+      <div
+        className=" bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5"
+        style={{ height: "20%", width: "20%" }}
+      >
         <img
           className="smaller"
           onMouseOver={() => change([day, text, link, directLink])}
           onClick={() => open(true)}
           alt="gallery"
-          style={{ height: "40%", width: "40%" }}
           src={process.env.PUBLIC_URL + link}
         />
         <div></div>
