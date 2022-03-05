0a9efa1946d3837e4672e594383c72f65dfc1e55
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-03-05 02:40:53 -0500

    changed color of footer text and changed h3 tags to p to pass last lightouse progressive app tests

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,21 @@ import React, { Fragment } from "react";
 const Footer = () => {
   return (
     <Fragment>
-      <div className="fixed vh-10 w-100 bottom-0 bg-light-red white-80 ph1">
+      <div className="fixed vh-10 w-100 bottom-0 bg-light-red black-80  ph1">
         <p className="f6">
-          <span className="dib mr4 mr5-ns">©2048 Your Company LLC, Inc.</span>
+          <span className="dib mr4 mr5-ns b">©2048 Your Company LLC, Inc.</span>
           <button className="f6 link dim ba ph3 pv2 mb2 dib black">
             Terms
           </button>{" "}
-          /
+          <span className=" b"> / </span>
           <button className="f6 link dim ba ph3 pv2 mb2 dib black">
             {" "}
             Privacy{" "}
           </button>{" "}
-          /
-          <h3 className=" f6 ph3 pv2 mb2 dib black ">
+          <span className="b"> /</span>
+          <p className=" f6 ph3 pv2 mb2 dib black b">
             Pictures belong to their respective owners{" "}
-          </h3>
+          </p>
         </p>
       </div>
     </Fragment>
